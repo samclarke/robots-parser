df58a648e3c9c1f0a59c786de20c66ed37e80154
Dan Habot <dan.habot@gmail.com>
2021-08-09 19:12:57 -0400

    forgot question marks

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,10 @@
 declare module 'robots-parser';
 
 interface Robot {
-  isAllowed(url: string, ua: [string]): boolean | undefined;
-  isDisallowed(url: string, ua: [string]): boolean | undefined;
-  getMatchingLineNumber(url: string, ua: [string]): number | undefined;
-  getCrawlDelay(ua: [string]): number | undefined;
+  isAllowed(url: string, ua?: [string]): boolean | undefined;
+  isDisallowed(url: string, ua?: [string]): boolean | undefined;
+  getMatchingLineNumber(url: string, ua?: [string]): number | undefined;
+  getCrawlDelay(ua?: [string]): number | undefined;
   getSitemaps(): [string];
   getPreferredHost(): string | null;
 }
