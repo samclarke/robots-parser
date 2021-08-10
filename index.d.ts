337fd97f3dc5063ae69f181a6aa6049dfd2f841b
Dan Habot <dan.habot@gmail.com>
2021-08-10 12:07:24 -0400

    fix

--- index.d.ts
+++ index.d.ts
@@ -1,11 +1,11 @@
 declare module 'robots-parser';
 
 interface Robot {
-  isAllowed(url: string, ua?: [string]): boolean | undefined;
-  isDisallowed(url: string, ua?: [string]): boolean | undefined;
-  getMatchingLineNumber(url: string, ua?: [string]): number | undefined;
-  getCrawlDelay(ua?: [string]): number | undefined;
-  getSitemaps(): [string];
+  isAllowed(url: string, ua?: string[]): boolean | undefined;
+  isDisallowed(url: string, ua?: string[]): boolean | undefined;
+  getMatchingLineNumber(url: string, ua?: string[]): number | undefined;
+  getCrawlDelay(ua?: string[]): number | undefined;
+  getSitemaps(): string[];
   getPreferredHost(): string | null;
 }
 
